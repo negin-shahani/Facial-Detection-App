1a80c728a1092056d6fe416b0326ae6f34410974
negin-titanium <neginshahany@gmail.com>
2022-08-22 18:53:24 +0430

    create a new image using the GD

--- app/Http/Controllers/GoogleAPIController.php
+++ app/Http/Controllers/GoogleAPIController.php
@@ -10,5 +10,7 @@ class GoogleAPIController extends Controller
     public function detectFaces(){
         $cloud = new ServiceBuilder(['keyFilePath' => base_path('facial-detection-app-KEY.json'), 'projectId' => 'facial-detection-app-360109' ]);   
         $vision = $cloud->vision();
+
+        $output = imagecreatefromjpeg(public_path('friends.jpg'));
     }
 }
