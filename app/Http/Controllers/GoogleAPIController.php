8b20175d77a84d352683fb2eccc52d3d3272c6e4
negin-titanium <neginshahany@gmail.com>
2022-08-22 18:23:39 +0430

    fix the bug of route

---
+++ app/Http/Controllers/GoogleAPIController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Google\Cloud\Core\ServiceBuilder;
+
+class GoogleAPIController extends Controller
+{
+    public function detectFaces(){
+        // Code will be added here   
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleAPIController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', 'GoogleAPIController@detectFaces');
+Route::get('/', [GoogleAPIController::class, 'detectFaces']);
