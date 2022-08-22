d53921ed2727d07c5c5b6f00354b1e285a48e372
negin-titanium <neginshahany@gmail.com>
2022-08-22 19:00:50 +0430

    loop through the found faces and draw boxes around the them

--- app/Http/Controllers/GoogleAPIController.php
+++ app/Http/Controllers/GoogleAPIController.php
@@ -16,5 +16,16 @@ class GoogleAPIController extends Controller
         $image = $vision->image(file_get_contents(public_path('friends.jpg')), ['FACE_DETECTION']);
         
         $results = $vision->annotate($image);
+
+        foreach ($results->faces() as $face){
+            $vertices = $face->boundingPoly()['vertices'];
+
+            $x1 = $vertices[0]['x'];
+            $y1 = $vertices[0]['y'];
+            $x2 = $vertices[2]['x'];
+            $y2 = $vertices[2]['y'];
+
+            imagerectangle($output, $x1, $y1, $x2, $y2, 0x00ff00);
+        }
     }
 }
